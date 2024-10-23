ddd01d6bf2ee612bf2e92576983b5c4ce3705765
John S <28509298+jsett@users.noreply.github.com>
2024-10-23 12:11:18 -0400

    change addTask to using an api route since revalidating causes bad performence

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,45 @@
+
+import prisma from "@/lib/db";
+
+export async function POST(request: Request) {
+    const formData = await request.formData()
+
+    let state: string | null | undefined = formData.get("state")?.toString();
+    let boardID: string | null | undefined = formData.get("board")?.toString();
+    let text: string | null | undefined = formData.get("text")?.toString();
+    let assignedUser: string | null | undefined = formData.get("assignedUser")?.toString();
+
+    console.log(`state: ${state} boardID: ${boardID} text: ${text} assignedUser: ${assignedUser}`)
+
+    if (state && boardID && assignedUser) {
+
+        const result = await prisma.tasks.create({
+            data: {
+                state: state,
+                dueDate: "-",
+                text: text,
+                board: {
+                    connect: {
+                        id: parseInt(boardID)
+                    }
+                },
+                user: {
+                    connect: {
+                        id: parseInt(assignedUser)
+                    }
+                }
+            },
+            include: {
+                user: true,
+                tagslist: {
+                    include: {
+                        tag: true
+                    }
+                }
+            }
+        })
+        return Response.json(result)
+    }
+    return Response.json({ error: "something when wrong in creating the task" })
+    
+  }
--- app/component/main/addTask.tsx
+++ app/component/main/addTask.tsx
@@ -1,8 +1,25 @@
 import { useRecoilState } from "recoil"
 import { addTaskAction } from "@/app/actions/addTaskAction"
+import { useAtom } from "jotai";
+import { tasksAtom } from "@/store/data";
 
 
 export default function AddTask({ boardID, state }){
+    const [tasks, setTasks] = useAtom(tasksAtom);
+
+    const submitForm = () => {
+        const form = new FormData(document.getElementById(`create_${state}`));
+        fetch('/api/tasks', {
+            method: 'post',
+            body: form,
+        }).then((res) => {
+            res.json().then((ret) => {
+                setTasks((pre) => {
+                    return [...pre, ret]
+                })
+            });
+        });
+    }
 
     return <>
         <div className="flex flex-row w-full justify-center" onClick={() => document.getElementById(`create_modal_${state}`).showModal()}>
@@ -16,7 +33,7 @@ export default function AddTask({ boardID, state }){
         <dialog id={`create_modal_${state}`} className="modal">
             <div className="modal-box">
                 <h3 className="font-bold text-lg">Add A Task</h3>
-                <form id={`create_${state}`} action={addTaskAction}>
+                <form id={`create_${state}`}>
                     <input type="hidden" name="state" value={state}/>
                     <input type="hidden" name="board" value={boardID}/>
                     <textarea name="text" className="textarea w-full textarea-bordered" placeholder="Task Description"></textarea>
@@ -25,7 +42,7 @@ export default function AddTask({ boardID, state }){
 
                 <div className="modal-action">
                     <form method="dialog">
-                        <button className="btn mr-2" onClick={() => document.getElementById(`create_${state}`).submit()}>Submit</button>
+                        <button className="btn mr-2" onClick={submitForm}>Submit</button>
                         <button className="btn">Cancel</button>
                     </form>
                 </div>
